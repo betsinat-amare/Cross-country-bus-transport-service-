479285e151b588e2559831ab2deeac474dbf5db9
Dawit Mekonnen Kassa <dkassa253@gmail.com>
2025-01-14 04:00:26 +0300

    modification

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get , Put , Post, Delete, Param, Body} from '@nestjs/common';
+import { Controller, Get , Put , Post, Delete, Param, Body, UseGuards} from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreatRoutDto } from '../auth/dto/createRout.dto';
-
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
+import { Role } from '@prisma/client';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 @Controller('routes')
 export class RoutesController {
     constructor(private readonly routeServices:RoutesService){}
@@ -18,18 +21,22 @@ async getRoute(
 ) {
     return this.routeServices.getRoute(departure,destination);
 }
-
+@UseGuards(AuthGuard('jwt'),RolesGuard)
+@Roles(Role.ADMIN)
 @Put(':id')
  async updateRoute(@Param('id') id:string, @Body()data:any) {
     return this.routeServices.updateRoute(parseInt(id, 10),data) ;
 }
 
+@UseGuards(AuthGuard('jwt'),RolesGuard)
+@Roles(Role.ADMIN)
 @Post('create')
 async createRoute(@Body() createRouteDto: CreatRoutDto) {
     return this.routeServices.createRoute(createRouteDto);
 }
 
-
+@UseGuards(AuthGuard('jwt'),RolesGuard)
+@Roles(Role.ADMIN)
 @Delete(':id')
  async deleteRoute(@Param('id')id:string){
     return this.routeServices.deleteRoute(parseInt(id));
